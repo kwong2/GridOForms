15a137d624845ccba4ffc4b87fba17555f60da17
Kevin Wong <k.wong4@yahoo.com>
2019-02-14 23:03:16 -0500

    jquery click alert displays but doubles

--- Forms/wwwroot/js/site.js
+++ Forms/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function(){
+    $(".box").click(function () {
+        alert("Hello");
+    });
+});
+
+// $(box).click(function() {
+//     alert("You've clicked this box yo!");
+// });
