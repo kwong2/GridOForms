218e080cae67168b4cddc9a9cf6b0e47044e47e5
Kevin Wong <k.wong4@yahoo.com>
2019-02-15 00:54:38 -0500

    added fields to Forms model

---
+++ Forms/Models/Form.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Forms.Models
+{
+    public class Form
+    {
+        public int FormId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+    }
+}
+
