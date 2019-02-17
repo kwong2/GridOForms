f7726289ecec9d64a5108b00433ca29fcbf5197e
Kevin Wong <k.wong4@yahoo.com>
2019-02-17 17:46:38 -0500

    switched default controller from Home to Forms

--- Forms/Startup.cs
+++ Forms/Startup.cs
@@ -56,7 +56,7 @@ namespace Forms
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Forms}/{action=Index}/{id?}");
             });
         }
     }
